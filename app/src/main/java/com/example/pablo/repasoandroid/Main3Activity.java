aa36fce0304cf2c1987d5ab100502c425c1ba6c8
Pablo Muguerza <pmuguer@gmail.com>
2018-05-26 08:06:22 -0300

    Agregado Toast para mostrar el producto

--- app/src/main/java/com/example/pablo/repasoandroid/Main3Activity.java
+++ app/src/main/java/com/example/pablo/repasoandroid/Main3Activity.java
@@ -2,15 +2,18 @@ package com.example.pablo.repasoandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.List;
+//import java.util.List;
 
 public class Main3Activity extends AppCompatActivity {
 
     private ListView listView;
-    private List<com.example.pablo.repasoandroid.Producto> productoList;
+    //private List<com.example.pablo.repasoandroid.Producto> productoList;
     private com.example.pablo.repasoandroid.ProductoAdapter myAdapter;
     private ArrayList listaProductos;
 
@@ -28,5 +31,15 @@ public class Main3Activity extends AppCompatActivity {
         myAdapter = new com.example.pablo.repasoandroid.ProductoAdapter(listaProductos);
         listView.setAdapter(myAdapter);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                com.example.pablo.repasoandroid.Producto producto =
+                        (com.example.pablo.repasoandroid.Producto)listView.getItemAtPosition(position);
+
+                Toast.makeText(Main3Activity.this, "Producto:" + producto.getNombre(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
